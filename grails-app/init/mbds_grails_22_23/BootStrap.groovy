bb6fd5ef053c83602a26c71912846094366b86c1
Yessine-iut <yessinebenelbey0305@gmail.com>
2022-10-16 17:18:22 +0200

    Revert "Revert "Heroku""
    This reverts commit ae497e1271b0d2c057f99be9b8ef2a6abb9444b6.

--- grails-app/init/mbds_grails_22_23/BootStrap.groovy
+++ grails-app/init/mbds_grails_22_23/BootStrap.groovy
@@ -1,7 +1,7 @@
 package mbds_grails_22_23
 
 import com.mbds.grails.Annonce
-import com.mbds.grails.Illustration
+
 import com.mbds.grails.Role
 import com.mbds.grails.User
 import com.mbds.grails.UserRole
